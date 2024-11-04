1e2c3a5b77062c80224635d971c5dfce01fea0a6
Chris Moynan <Chrismoynan@outlook.com>
2024-11-04 19:13:47 +0000

    added home app and templates

--- gymstitute/settings.py
+++ gymstitute/settings.py
@@ -27,9 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['8000-cmoynan-gymstitute-bx4n3h4ydrh.ws.codeinstitute-ide.net']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-cmoynan-gymstitute-bx4n3h4ydrh.ws.codeinstitute-ide.net']
-
-
 
 # Application definition
 
@@ -44,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +60,10 @@ ROOT_URLCONF = 'gymstitute.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
