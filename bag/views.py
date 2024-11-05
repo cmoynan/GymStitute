72578cddc7512a2200721a8d1a2c93cacbb8a548
Chris Moynan <Chrismoynan@outlook.com>
2024-11-05 14:51:01 +0000

    added remove items from bag option

--- bag/views.py
+++ bag/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
-
 def view_bag(request):
     """ A view that renders the bag contents page """
-
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -22,4 +18,14 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
-    
+
+def remove_from_bag(request, item_id):
+    """Remove the specified item from the shopping bag"""
+    bag = request.session.get('bag', {})
+
+    # Remove the item if it exists in the bag
+    if item_id in bag:
+        del bag[item_id]
+        request.session['bag'] = bag
+
+    return redirect('view_bag')
