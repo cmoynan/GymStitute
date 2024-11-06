448f84b74eefec85e0fc3743dafee1fbb9bf64ec
Chris Moynan <Chrismoynan@outlook.com>
2024-11-06 14:19:39 +0000

    bug fixed where items with sizes were renderingas a dictionary in the template view . fixed in bag contexts. Updted styling for the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -64,11 +64,10 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
-                                quantity=quantity,
                                 product_size=size,
                             )
                             order_line_item.save()
