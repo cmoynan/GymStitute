27c47bd2d8c389497cef061b4bc647cb36ecaf61
Chris Moynan <Chrismoynan@outlook.com>
2024-11-09 19:28:59 +0000

    added robots.txt file

--- gymstitute/urls.py
+++ gymstitute/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +26,8 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-     path('profile/', include('profiles.urls')),
-     path('about/', include('about.urls')),
+    path('profile/', include('profiles.urls')),
+    path('about/', include('about.urls')),
+    path('robots.txt', serve, {'path': 'robots.txt', 'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
