c85ba2b53bda1ad6e26bbdacb4e41667dc963f66
Chris Moynan <Chrismoynan@outlook.com>
2024-11-10 21:34:12 +0000

    Add message container to base template - Style messages for different alert types (success, error, warning, info) - Add responsive CSS for mobile display - Position messages fixed to top-right of viewport

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -14,23 +15,34 @@ def add_to_bag(request, item_id):
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
+    # Check if size is provided, and update the session bag accordingly
     if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                # Add a success message
+                messages.success(request, f'{quantity} x {size}  added to your bag!')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                # Add a success message
+                messages.success(request, f'{quantity} x {size}  added to your bag!')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            # Add a success message
+            messages.success(request, f'{quantity} {size} of the product added to your bag!')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            # Add a success message
+            messages.success(request, f'{quantity} more of the product added to your bag!')
         else:
             bag[item_id] = quantity
+            # Add a success message
+            messages.success(request, f'{quantity} of the product added to your bag!')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
+
 def remove_from_bag(request, item_id):
     """Remove the specified item from the shopping bag"""
     bag = request.session.get('bag', {})
@@ -39,5 +51,11 @@ def remove_from_bag(request, item_id):
     if item_id in bag:
         del bag[item_id]
         request.session['bag'] = bag
+        # Add a success message
+        messages.success(request, 'Item successfully removed from your bag!')
+    else:
+        # If the item does not exist in the bag, show an error message
+        messages.error(request, 'Item not found in your bag.')
 
     return redirect('view_bag')
+
